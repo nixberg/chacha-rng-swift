7b8c3f1d60fd92e4b6f3abe43ce1771060b24ea7
nixberg <310825+nixberg@users.noreply.github.com>
2023-05-04 19:46:59 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
-        .package(url: "https://github.com/nixberg/chacha-swift", branch: "main"),
+        .package(url: "https://github.com/nixberg/chacha-swift", "0.1.0"..<"0.2.0"),
     ],
     targets: [
         .target(
